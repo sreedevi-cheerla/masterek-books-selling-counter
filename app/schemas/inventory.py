5a9617b3974113c5c98fd89823e43f64ea2c06d7
sreedevi-cheerla <sreedevi.cheerla@gmail.com>
2026-09-08 11:09:56 +0530

    Update inventory.py

--- app/schemas/inventory.py
+++ app/schemas/inventory.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class BookBase(BaseModel):
     name: str
@@ -7,6 +7,11 @@ class BookBase(BaseModel):
     original_price: float
     discount_percentage: float
     available_copies: int
+
+class BookCreate(BookBase):
+    original_price: float = Field(gt=0)
+    discount_percentage: float = Field(ge=0, le=100)
+    available_copies: int = Field(ge=0)
 
 class BookResponse(BookBase):
     id: int
