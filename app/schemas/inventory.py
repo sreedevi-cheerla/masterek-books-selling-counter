f8f83559f41f459e7f3f1b72d104afac21ecf701
sreedevi-cheerla <sreedevi.cheerla@gmail.com>
2026-09-08 10:37:19 +0530

    Update inventory.py

--- app/schemas/inventory.py
+++ app/schemas/inventory.py
@@ -1,1 +1,15 @@
+from pydantic import BaseModel
 
+class BookBase(BaseModel):
+    name: str
+    language: str
+    category: str
+    original_price: float
+    discount_percentage: float
+    available_copies: int
+
+class BookResponse(BookBase):
+    id: int
+
+    class Config:
+        from_attributes = True
